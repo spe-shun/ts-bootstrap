d9ad236787eb4ca712875e701af11f1540d5329b
spe-shun <shunyu.1994@gmail.com>
2023-10-08 11:05:16 +0800

    feat: 初版

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,81 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import * as childProcess from 'child_process';
+import * as path from 'path';
 import * as vscode from 'vscode';
 
+const TERMINAL_NAME = 'ts-node Terminal';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('ts-bootstrap.runAtTsNodeTerminal', () => {
+        // 找之前有没有启动过 该 Terminal
+        let terminal = vscode.window.terminals.find(t => t.name === TERMINAL_NAME);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ts-bootstrap" is now active!');
+        if (!terminal) {
+            terminal = vscode.window.createTerminal({ name: TERMINAL_NAME });
+        }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ts-bootstrap.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ts-bootstrap!');
-	});
+        const tsVilidResult = childProcess.execSync('which ts-node').toString();
 
-	context.subscriptions.push(disposable);
+        if (/ts-node\snot\sfound/.test(tsVilidResult)) {
+            vscode.window.showInformationMessage('请先运行 npm i -g ts-node');
+            return;
+        }
+
+        const activeTextEditor = vscode.window.activeTextEditor;
+
+        if (activeTextEditor) {
+            const baseName = activeTextEditor.document.fileName;
+
+            const pathName = path.dirname(baseName);
+            const fileName = path.basename(baseName);
+
+            terminal.show();
+            terminal.sendText(`cd ${pathName} && ts-node ${fileName}`);
+        } else {
+            vscode.window.showInformationMessage('未选中任何文件');
+        }
+
+        // 在终端中执行命令
+
+        // // 显示终端
+        // terminal.show();
+        // // 获取当前活动的调试会话;
+        // const activeDebugSession = vscode.debug.activeDebugSession;
+
+        // if (activeDebugSession) {
+        //     // 发送自定义命令给 JavaScript 调试会话
+        //     activeDebugSession
+        //         .customRequest('yourCustomRequestName')
+        //         .then(response => {
+        //             // 处理响应
+        //             vscode.window.showInformationMessage(
+        //                 'Received response from JavaScript Debug Session: ' + JSON.stringify(response),
+        //             );
+        //         })
+        //         // .catch(error => {
+        //         //     // 处理错误
+        //         //     vscode.window.showErrorMessage(
+        //         //         'Error communicating with JavaScript Debug Session: ' + error.message,
+        //         //     );
+        //         // });
+        // } else {
+        //     vscode.window.showInformationMessage('No active JavaScript Debug Session found.');
+        // }
+        // // 配置要启动的调试器
+        // const debugConfiguration = {
+        //     type: 'node',
+        //     request: 'attach',
+        //     name: 'Attach to Process',
+        //     port: 9229, // 连接到的进程的调试端口
+        // };
+
+        // // 启动调试 Attach 模式
+        // vscode.debug.startDebugging(undefined, debugConfiguration);
+    });
+
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
